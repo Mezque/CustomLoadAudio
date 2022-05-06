bd33133a11c5e0cb3b86fdd0d9b8714adb4697b5
Mezque <31026406+Mezque@users.noreply.github.com>
2022-05-06 16:58:33 -0300

    Pr changes
    1. My original idea to make the toggle to use a custom path or user data was kind of stupid, so I didn't make that.
    
    2. Readded back Assembly info
    
    3. Made mod author purple again lol
    
    4. Set the default location up to be userdata\CustomLoadAudio
    
    5. Created the folder Userdata\CustomLoadAudio if its none existent.
    
    Co-Authored-By: Dotlezz <103538714+dotlezz@users.noreply.github.com>

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomLoadAudio")]
+[assembly: AssemblyDescription("A melonloader mod that changes the load audio in vrchat")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mezque")]
+[assembly: AssemblyProduct("CustomLoadAudio")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("Mezque")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(CustomLoadingAudio.Main), "CustomLoadAudio", $"{AssemblyInfo.Version}", "Mezque + Dotlezz")]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonColor(System.ConsoleColor.DarkMagenta)]
+[assembly: MelonAuthorColor(System.ConsoleColor.Magenta)]
+[assembly: ComVisible(false)]
+[assembly: Guid("3adc9acc-d839-403b-bae0-c114293d7dda")]
+[assembly: AssemblyVersion($"{AssemblyInfo.Version}")]
+[assembly: AssemblyFileVersion($"{AssemblyInfo.Version}")]
+
+internal struct AssemblyInfo
+{
+    internal const string Version = "2.0.0.0";
+}
